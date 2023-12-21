ad7a56558a59f59bd40aa127838705f77bc4f5a2
Ramil Albakov <ramil.albakov2@gmail.com>
2023-12-21 22:18:02 +0600

    fix: add default value for third cli parameter

--- gendiff/cli.py
+++ gendiff/cli.py
@@ -15,5 +15,11 @@ def parse_cli_args():
     )
     parser.add_argument('first_file', type=str)
     parser.add_argument('second_file', type=str)
-    parser.add_argument('-f', '--format', type=str, help='set format of output')
+    parser.add_argument(
+        '-f',
+        '--format',
+        type=str,
+        help='set format of output',
+        default='stylish',
+    )
     return parser.parse_args()
